8003941bae230b045df94872c94c12ee9d5a683b
gaurav <gauravg77061@gmail.com>
2026-01-22 21:16:42 +0530

    String

---
+++ tut1.py
@@ -0,0 +1,9 @@
+print("Hello world")
+
+chai_description="Aromatic  and  Bold"
+
+print(f"First order: {chai_description} please!" )
+
+print(f'First order: {chai_description[1:7:2]} please give me')
+
+print(f'First order :{chai_description[::-1]} doing revrsing ')
