6c173be9b0c4f0f792919f746df11b723f749340
gaurav <gauravg77061@gmail.com>
2026-01-27 23:01:00 +0530

    all about tuples and ist

--- tut1.py
+++ tut1.py
@@ -1,9 +1,35 @@
-print("Hello world")
+# print("Hello world")
 
-chai_description="Aromatic  and  Bold"
+# chai_description="Aromatic  and  Bold"
 
-print(f"First order: {chai_description} please!" )
+# print(f"First order: {chai_description} please!" )
 
-print(f'First order: {chai_description[1:7:2]} please give me')
+# print(f'First order: {chai_description[1:7:2]} please give me')
 
-print(f'First order :{chai_description[::-1]} doing revrsing ')
+# print(f'First order :{chai_description[::-1]} doing revrsing ')
+
+# masala_spices=("clove","cinaamon" ,"spice33")
+# print(f"spices are",masala_spices)
+# (spice1,spice2,spice3)=masala_spices
+# print(f"spices are",spice1);
+
+# ginger_ratio,caradom_ratio=2,1
+# print(f"Ratio of G:{ginger_ratio} and C:{caradom_ratio}");
+# ginger__ratio,caradom_ratio=caradom_ratio, ginger_ratio
+# print(f"Ratio of g:{ginger__ratio} and c:{caradom_ratio}")
+
+# ingredients=["water","tea","sugar","clove"]
+# print(f"list elements are:{ingredients}")
+# ingredients.remove("sugar")
+# print(f"list of elements {ingredients}")
+# ingredients.append("sugar")
+# print(f"elements in list {ingredients}")
+# ingredients.insert(2,"masala tea")
+# print(f"elements in the list are {ingredients}")
+# ingredients.reverse()
+# print(f"reverse kist is {ingredients}")
+# ingredients.sort()
+# print(f"thi is the sorted lsit {ingredients}")
+
+
+
