d95762a9c5d0df414354ba701577bf310a50932f
gaurav <gauravg77061@gmail.com>
2026-02-02 23:51:33 +0530

    userStory 3

--- userStory.py
+++ userStory.py
@@ -7,9 +7,21 @@
 
 # user story 2
 
-snack=input("Enter your preferred snack: ").lower()
+# snack=input("Enter your preferred snack: ").lower()
 
-if snack == "cookies" or  snack == "samosa":
-    print(f"Great choice will serve you {snack} soon!")
+# if snack == "cookies" or  snack == "samosa":
+#     print(f"Great choice will serve you {snack} soon!")
+# else:
+#     print("sorry we only offer samosa and cookies")
+
+#user story - 3 
+
+cup_size=input("enter the size of cup").lower()
+
+if cup_size=="small":
+    print(f"the price of small cup is 10")
+elif cup_size =="medium":
+    print("the price of medium cup is 15Rs")
 else:
-    print("sorry we only offer samosa and cookies")
+    print("The price of large cup is 20")
+
