087ab5b6e5fb91fcf15dffef01cb118bdbe41fcc
gaurav <gauravg77061@gmail.com>
2026-02-02 19:07:42 +0530

    operator overloading and bytearray

---
+++ tut2.py
@@ -0,0 +1,12 @@
+
+base__liquid=['water','milk']
+extra_flavour=['ginger']
+
+full_liquid=base__liquid+extra_flavour
+print((f"this is method overloading, {full_liquid}"))
+
+strong_brew=["black tea",'water']*3
+print(f"operator operloading {strong_brew}")
+
+raw_spice_data=bytearray(b"cinnamon")
+print(f"elements is byter array {raw_spice_data}")
