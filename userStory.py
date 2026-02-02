b36759188683cc56e8a0e73b9d7ba12d8350ff96
gaurav <gauravg77061@gmail.com>
2026-02-02 23:31:22 +0530

    user story 2

---
+++ userStory.py
@@ -0,0 +1,15 @@
+#user story 1 
+
+# kettle_boiled=True
+
+# if kettle_boiled == True:
+#     print("kettle done ! timme to make chai")
+
+# user story 2
+
+snack=input("Enter your preferred snack: ").lower()
+
+if snack == "cookies" or  snack == "samosa":
+    print(f"Great choice will serve you {snack} soon!")
+else:
+    print("sorry we only offer samosa and cookies")
