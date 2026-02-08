664785384ce7393f8350fea9af801ce908dcc14c
gaurav <gauravg77061@gmail.com>
2026-02-08 15:42:48 +0530

    yieldfrom and close()

--- userStory.py
+++ userStory.py
@@ -325,6 +325,36 @@
 # next(stall)
 
 # stall.send("Masal chai")
+
+# yield fromand close the generators
+
+def local_chai():
+    yield "Masal chai"
+    yield "ginger chai"
+
+def imported_chai():
+    yield "Matcha"
+    yield "Oolong"
+
+def full_menu():
+    yield from local_chai()
+    yield from imported_chai()
+
+for chai in full_menu():
+    print(chai)
+
+
+def chai_stall():
+    try:
+        while True:
+            order = yield "Waiting chai order"
+    except:
+        print("Stall closed,No more chai")
+
+stall =chai_stall()
+print(next(stall))
+stall.close()
+
   
 
 
