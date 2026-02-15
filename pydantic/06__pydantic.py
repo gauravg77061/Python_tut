d02aa390066afe9f7e8a50228ea129da78ee2c49
gaurav <gauravg77061@gmail.com>
2026-02-15 16:42:02 +0530

    computed fields

---
+++ pydantic/06__pydantic.py
@@ -0,0 +1,36 @@
+from pydantic import BaseModel,computed_field,Field
+
+class Product(BaseModel):
+    price:float
+    quantity:int
+
+    @computed_field
+    @property
+    def total_price(self) -> float:
+        return self.price*self.quantity
+    
+class Booking(BaseModel):
+    user_id:int
+    room_id:int
+    night:int =Field(
+        ...,
+        ge=1
+    )
+    rate_per_night:float
+    @computed_field
+    @property
+    def tot_amount(self) -> float:
+        return self.night*self.rate_per_night
+
+
+booking=Booking(
+    user_id=123,
+    room_id=345,
+    night=3,
+    rate_per_night=100.0
+
+)
+print(booking.tot_amount)
+print(booking.model_dump())
+    
+    
