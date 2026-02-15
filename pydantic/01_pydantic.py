67fb7489b55e0b5d9e2047a88cc29362c9916eaa
gaurav <gauravg77061@gmail.com>
2026-02-15 14:40:55 +0530

    pydantic

---
+++ pydantic/01_pydantic.py
@@ -0,0 +1,14 @@
+from pydantic import BaseModel
+
+class User(BaseModel):
+    id:int
+    name:str
+    is_active:bool
+
+input__data={'id':101,'name':'chaicode','is_active':True}
+input_data1={'id':102,'name':123,"is_active":True}
+user=User(**input__data)
+user1=User(**input_data1)
+print(user)
+print(user1)
+
